ff5c79b96d3a912180e9fe66cfbaf3369cd813f9
Timothy Cevallos <timcev@gmail.com>
2017-10-11 18:28:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,13 @@ function getRepositories() {
   req.send()
 }
 
-function getBranches(){
-  
+function getBranches(el){
+  let branch = el.dataset.repository
+  let user = document.getElementById('username').value
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayBranches);
+  req.open("GET", `${'https://api.github.com/repos/' +user+ '/' +branch+'/branches'}`)
+  req.send()
 }
 function getCommits(el) {
   const name = el.dataset.repo
