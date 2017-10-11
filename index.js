a22a3f07d4c8a022e052c8d51fda2aed585e237d
Timothy Cevallos <timcev@gmail.com>
2017-10-11 18:22:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function displayRepositories(event, data) {
+function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
