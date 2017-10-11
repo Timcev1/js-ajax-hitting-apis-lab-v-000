b50ce60ce7fbb9d51a2d881c020f54d4b79ab5c5
Timothy Cevallos <timcev@gmail.com>
2017-10-11 18:33:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function displayRepositories(event, data) {
 function getRepositories() {
   let user = document.getElementById('username').value
   const req = new XMLHttpRequest()
-  req.addEventListener("load", showRepositories);
+  req.addEventListener("load", displayRepositories);
   req.open("GET", `${'https://api.github.com/users/' + user + '/repos'}`)
   req.send()
 }
