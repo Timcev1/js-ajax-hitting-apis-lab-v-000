925c620c63510e5a60765ce2408ad5a265580c16
Timothy Cevallos <timcev@gmail.com>
2017-10-11 18:17:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function showRepositories(event, data) {
 function getRepositories() {
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.open("GET", 'https://api.github.com/users/' + user +'/repos')
   req.send()
 }
 
